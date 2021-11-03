a0d3782dd06b36499d66fbff84bb32d12acca4e3
JIdayyy <julien_abbadie@hotmail.fr>
2021-11-03 04:26:32 +0100

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/archivo";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { toast, ToastContainer } from "react-toastify";
+import "nprogress/nprogress.css";
+
+const theme = extendTheme({
+    fonts: {
+        heading: "Archivo",
+        body: "Archivo",
+    },
+    colors: {
+        purple: {
+            default: "#8C65CE",
+        },
+    },
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const notify = () => toast("Hey Bienvenue à toi !");
+    notify();
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+            <ToastContainer autoClose={3000} />
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
