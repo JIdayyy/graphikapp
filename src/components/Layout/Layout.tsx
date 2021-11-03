120472fb44d891c349ddb82600cca9f27a54bc1f
JIdayyy <julien_abbadie@hotmail.fr>
2021-11-03 21:43:26 +0100

    PWA config

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -22,9 +22,29 @@ export default function Layout({ children }: IProps): ReactElement {
             left={0}
         >
             <Head>
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <meta name="description" content="Description" />
+                <meta name="keywords" content="Keywords" />
+                <title>Next.js PWA Example</title>
                 <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/icons/favicon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="/icons/favicon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="theme-color" content="#8C65CE" />
                 <meta
                     name="apple-mobile-web-app-status-bar"
